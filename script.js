2939d284c22ead8ad1d941993272ecc787402543
swdchoi <sungwonchoi224@gmail.com>
2024-11-26 17:27:12 +0800

    keyboard number and rules can control calculator

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ const zero = document.getElementById("0");
 const point = document.getElementById(".");
 const equal = document.getElementById("=");
 
-const output = document.getElementById("output")
+const output = document.getElementById("output");
 const clear = document.getElementById("clear");
+const back = document.getElementById("back");
 
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
@@ -50,7 +51,6 @@ const rulesPress = (rule) => {
 if (firstNum == false) {
 calculate();
 ruleUse = rules[rule].innerText;
-//output.innerHTML = `${rules[rule].innerText}`;
 } 
 else {
 ruleUse = rules[rule].innerText;
@@ -110,6 +110,49 @@ const clearing = () =>{
      numStoreTwo = [];
 }
 
+const backCondition = () => {
+    if(numStoreOne[0] == "0") {
+        numStoreOne = [0]
+        output.innerHTML = numStoreOne.toString().replace(/,/g, "");
+    } else if (numStoreTwo[0] == "0") {
+        numStoreTwo = [0]
+        output.innerHTML = numStoreTwo.toString().replace(/,/g, "");
+    } else {
+        backOne();
+    }
+}
+
+const backOne = () =>{
+if(firstNum == true) {
+    numStoreOne.pop();
+    output.innerHTML = numStoreOne.toString().replace(/,/g, "");
+    }
+    else if (firstNum == false) {
+    numStoreTwo.pop();
+    output.innerHTML = numStoreTwo.toString().replace(/,/g, "");
+    }
+}
+
 clear.addEventListener("click", () => {clearing()});
 equal.addEventListener("click", () => {calculate()});
+back.addEventListener("click", () => {backCondition()});
 document.addEventListener("DOMContentLoaded", () => {onload()});
+document.addEventListener("keydown", (e) => {
+    if(0 <= e.key && e.key <= 9){
+        pushNumber(e.key);}
+     else if(e.key == ".") {
+        pushNumber(10);
+     } else if (e.key == "+"){
+        rulesPress(0)
+     } else if (e.key == "-") {
+        rulesPress(1)
+      } else if (e.key == "*"){
+        rulesPress(2) 
+      } else if (e.key == "/") {
+        rulesPress(3)
+      } else if (e.key == "Enter"){
+        calculate();
+      } else if (e.key == "Backspace") {
+        backCondition();
+      }
+    })
